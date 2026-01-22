691c8edbf1f101f0e5955cecfcdce8d04e306936
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2026-01-21 20:06:05 -0600

    chore: delete existing react app

--- src/Constants.js
+++
@@ -1,7 +0,0 @@
-export const TSUMAMI = {
-    project: "https://github.com/AMMRS-Capstone-TSUMAMI/tsumami-jalopy",
-    site: "https://www.tsumami.cooking/landing",
-    image: "/tsumami.png",
-    title: "tsUmami",
-    description: "Meal planner; calorie tracker; and recipe repository."
-}
